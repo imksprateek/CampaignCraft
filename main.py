51b214399ca55dce3630c263637be3a541637dc8
imksprateek <ksprateek2004@gmail.com>
2024-09-29 18:38:50 +0530

    Integrated text model to structure user requirements into a refined prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import base64
 import boto3,json
 import os
 from flask import Flask, request, jsonify, Response, redirect, send_file
+from botocore.exceptions import ClientError
 import uuid
 
 app = Flask(__name__)
@@ -12,11 +13,40 @@ def generate_image():
     query_dict = request.args.to_dict(flat=False)
 
     prompt = query_dict.get('prompt', [None])[0]
+    
+    client = boto3.client("bedrock-runtime", region_name="us-east-1")
+
+    model_id = "anthropic.claude-v2"
+
+    user_message = f"You are a prompt engineer. Design a prompt for Image generation AI for the following requirement. Make sure you don't include any sensitive words that the image AI might not generate. You are generating a background image with no text. You should only have the prompt in your response: {prompt}"
+    conversation = [
+        {
+            "role": "user",
+            "content": [{"text": user_message}],
+        }
+    ]
+
+    try:
+        response = client.converse(
+            modelId="anthropic.claude-v2",
+            messages=conversation,
+            inferenceConfig={"maxTokens":2400,"stopSequences":["\n\nHuman:"],"temperature":1,"topP":1},
+            additionalModelRequestFields={"top_k":250}
+        )
+
+        response_text = response["output"]["message"]["content"][0]["text"]
+        print(response_text)
+
+    except (ClientError, Exception) as e:
+        print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+        return jsonify({'error': 'Error in generating prompt from Text model', 'reason':f'Model id: {model_id}, {e}'})
+    
+    
     client = boto3.client("bedrock-runtime", region_name="us-east-1")
     
     model_id = "stability.stable-diffusion-xl-v1"
 
-    native_request = {"text_prompts":[{"text":f"{prompt}","weight":1}],"cfg_scale":10,"steps":50,"seed":0,"width":1024,"height":1024,"samples":1}
+    native_request = {"text_prompts":[{"text":f"{response_text}","weight":1}],"cfg_scale":10,"steps":50,"seed":0,"width":1024,"height":1024,"samples":1}
 
     model_request = json.dumps(native_request)
 
@@ -43,6 +73,12 @@ def generate_image():
     
     return jsonify({'current': f"https://s3.amazonaws.com/{BUCKET}/{filename}", 'images': image_urls})
 
+
+@app.route('/speech', methods=['get'])
+def detect_speech():
+    query_dict = request.args.to_dict(flat=False)
+
+    prompt = query_dict.get('prompt', [None])[0]
 
 if __name__ == '__main__':
 	app.run(debug = True)
